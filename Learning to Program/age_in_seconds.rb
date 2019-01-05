7e4332f2398c0cad73bdc7f292ef9f39eacc5906
Cadowyn <JamesChanceWilson@gmail.com>
2019-01-05 11:50:06 -0500

    Add files via upload

---
+++ Learning to Program/age_in_seconds.rb
@@ -0,0 +1,18 @@
+# I'm 33 years old
+
+# 33 years 
+
+# Find seconds in a year
+
+# 60 seconds in a minute
+
+# __ seconds in an hour
+
+#.__ seconds in a day
+
+# __ seconds in a year
+
+# __ seconds for my age
+
+puts 60 * 60 * 24 * 365 * 33
+
---
+++ Learning to Program/hours_in_year.rb
@@ -0,0 +1,4 @@
+# How many hours are there in a year?
+
+puts 24 * 365
+
