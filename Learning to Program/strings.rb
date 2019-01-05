88c9204730307631d7917f41cd2f0a13c1c039dd
Cadowyn <JamesChanceWilson@gmail.com>
2019-01-05 12:27:34 -0500

    Add files via upload

---
+++ Learning to Program/strings.rb
@@ -0,0 +1,39 @@
+puts 'Hello'
+puts 'Ruby rocks.'
+puts 'Nobody deserves a mime, Buffy.'
+puts 'Snoopy says #%^&*@! when he stubs his toe.'
+puts '     '
+puts ''
+
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+
+# 3. 1 String arithmetic
+
+puts 'I like' + 'apple pie.'
+
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink' * 4
+
+# 3.2 12 vs '12'
+
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# 3.3 Problems
+
+# puts '12' + 12
+# puts '2' * '5'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of the string: \\'
+puts 'up\\down'
+puts 'up\down'
