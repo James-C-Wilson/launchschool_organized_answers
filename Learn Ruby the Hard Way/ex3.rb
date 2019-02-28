f46b6b6cf08d868b86bb3e13f4c68839a106d4e4
Cadowyn <JamesChanceWilson@gmail.com>
2019-02-28 15:32:18 -0500

    Add files via upload

---
+++ Learn Ruby the Hard Way/ex3.rb
@@ -0,0 +1,63 @@
+
+
+# +  plus (addition)
+
+# -  minus (subtraction)
+
+# /  slash (division)
+
+# *  asterisk (multiplication)
+
+# %  percent (modulo; the remainder)
+
+# <  less-than (is something less than)
+
+# >  greater-than (is something greater than)
+
+# <= less-than-equal (is something less than or equal to)
+
+# >= greater-than-equal (is something greater than or equal to)
+
+# This line puts to the screen the sentence.
+puts "I will now count my chickens:"
+
+# This line solves the math equation within the string
+puts "Hens #{25 + 30 / 6}"
+# This line counts the rooster by doing the math 
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# standard math problem
+puts "Now I will count the eggs:"
+
+# same as line 30
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# puts the the screen the stuff in ""
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#math equation
+puts 3 + 2 < - 7
+
+# puts the screen the sentence and solves in the #{}
+puts "What is 3 + 2? #{3 + 2}"
+# same as 42
+puts "What is 5 - 7? #{5 - 7}"
+
+# puts to the screen what's in the ""
+puts "Oh, that's why it's false."
+
+# same as 47
+puts "How about some more."
+
+# puts to the screen and checks if f > 2
+puts "Is it greater? #{5 > -2}"
+# puts to the screen and checks if 5 is greater than or equal to -2
+puts "Is it greater or equal? #{5 >= -2}"
+# puts to the screen and checks if 5 is less than or equal to negative 2
+puts "Is it less or equal? #{ 5 <= -2}"
+
+
+
+
+
+
